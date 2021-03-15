3fab5b9f0f4115f78e88b0754f969c86d982c9c4
Jonks <anastasiablackhold@gmail.com>
2021-03-15 15:34:25 -0700

    added useState functionality to set current day.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,24 +1,25 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
 export default function DayList(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
 
   return (days.map(day => {
     return (<DayListItem
