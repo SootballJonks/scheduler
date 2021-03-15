5022144e949e6c89d26c5844dab67c16b32fa8f4
Jonks <anastasiablackhold@gmail.com>
2021-03-15 15:18:32 -0700

    Built DayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+
+  return (days.map(day => {
+    return (<DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay} />)
+  }));
+}
