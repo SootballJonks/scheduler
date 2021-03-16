fd97d0f95608ac2df9a2fb9d4d8db07b61662707
Jonks <anastasiablackhold@gmail.com>
2021-03-16 14:04:08 -0700

    built interviewList component.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,33 @@
 import React from "react";
 
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
 export default function InterviewerList(props) {
   
+  const list = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        {...interviewer}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={(event) => {props.setInterviewer(interviewer.id)}}
+      />
+    )
+  })
   
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{list}</ul>
     </section>
   )
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -14,13 +14,13 @@ export default function InterviewerListItem(props) {
 
 
   return (
-    <li className={interviewerClass}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   )
 }
