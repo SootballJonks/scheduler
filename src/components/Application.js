4bbdd3132d670570ee061123bd7df7a7c25805e2
Jonks <anastasiablackhold@gmail.com>
2021-03-17 21:42:51 -0700

    Implemented axios HTTP requests.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments = [
   {
@@ -75,6 +60,16 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const url = "/api/days";
+
+    axios.get(url).then(response => {
+      setDays(response.data);
+    })
+  }, [])
+  
 
   return (
     <main className="layout">
