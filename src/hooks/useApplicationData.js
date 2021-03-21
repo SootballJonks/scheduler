18ecd7ffb1a53757300309f1fe088b5fcb8dabdf
Jonks <anastasiablackhold@gmail.com>
2021-03-21 11:24:54 -0700

    newSpots function added

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,6 +10,32 @@ export default function useApplicationData() {
     interviewers: {}
   })
 
+  const newSpots = (state, appointments) => {
+    console.log("State.days: ", state.days);
+
+    let dayID = state.days.filter((item) => {
+      return item.name === state.day;
+    })[0].id;
+
+    const appointmentsForDay = state.days[dayID - 1].appointments;
+
+    let spots = 0;
+
+    for (let appt of appointmentsForDay) {
+      if (appointments[appt].interview === null) {
+        spots++;
+      }
+    }
+
+    const days = state.days.map((day) => {
+      if (day.id === dayID) {
+        return { ...day, spots }
+      }
+      return day;
+    })
+
+    return days;
+  }
   
   const bookInterview = (id, interview) => {
     console.log(id, interview);
@@ -21,20 +47,36 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
-    setState({
-      ...state,
-      appointments
-    });
+    // setState({
+    //   ...state,
+    //   appointments
+    // });
+    const days = newSpots(state, appointments);
+
     return axios.put(`api/appointments/${id}`, appointment)
     .then((response) => {
       console.log("Added to database: ", response);
+      setState({...state, appointments, days });
     })
   }
   
   const cancelInterview = (id) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+
+    const days = newSpots(state, appointments);
+
     return axios.delete(`/api/appointments/${id}`, null)
     .then((response) => {
       console.log("Deleted from database: ", response);
+      setState({...state, appointments, days });
     })
   }
   
